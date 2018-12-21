2aa8d8e12c7642e78b1165d5f9127499bc86add9
takafukuyuya <45476836+takafukuyuya@users.noreply.github.com>
2018-12-21 14:03:40 +0900

    Update default.js

--- default.js
+++ default.js
@@ -149,8 +149,8 @@
                 gl.bindTexture(gl.TEXTURE_2D, tex);// テクスチャをバインド
                 // テクスチャへ画像を写す
                 gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.MIRROR);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.REPEAT);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
                 gl.bindTexture(gl.TEXTURE_2D, null);// バインドを外す
